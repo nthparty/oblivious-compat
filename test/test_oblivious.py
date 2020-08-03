98b9da26cf9bf70d7c2d16b1952001593a885f2d
Andrei Lapets <a@lapets.io>
2020-08-02 21:15:07 -0400

    Revert.

--- test/test_oblivious.py
+++ test/test_oblivious.py
@@ -15,85 +15,37 @@ def check_or_generate_operation(self, fun, lengths, bits):
     fs = fountains(sum(lengths), seed=0, limit=256, bits=bits, function=fun)
     return check_or_generate(self, fs, bits)
 
-class Test_oblivious_native(TestCase):
+class Test_oblivious(TestCase):
     def test_scalar(self, bits='4df8fe738c097afa7f255b10c3ab118eeb73e38935605042ccb7581c73f1e5e9'):
         def fun(bs):
-            return bitlist([1 if native.scalar(bs) else 0])
+            return bitlist([1 if scalar(bs) else 0])
         return check_or_generate_operation(self, fun, [32], bits)
 
     def test_base(self, bits='080874618c0878927620101043a31002e840818101204000401210101261c120'):
         def fun(bs):
-            return native.base(bs) if native.scalar(bs) else bytes([0])
+            return base(bs) if scalar(bs) else bytes([0])
         return check_or_generate_operation(self, fun, [32], bits)
 
     def test_mul(self, bits='28c5004000000100850000002102088891100000000081080810004280080004'):
         def fun(bs):
             (bs1, bs2) = parts(bs, length=32)
-            return\
-                native.mul(bs1, native.base(bs2))\
-                if native.scalar(bs1) and native.scalar(bs2) else\
-                bytes([0])
+            return mul(bs1, base(bs2)) if scalar(bs1) and scalar(bs2) else bytes([0])
         return check_or_generate_operation(self, fun, [32, 32], bits)
 
     def test_add(self, bits='0844000040000002818040008400000031080028000081080801000204081800'):
         def fun(bs):
             (bs1, bs2) = parts(bs, length=32)
-            return\
-                native.add(native.base(bs1), native.base(bs2))\
-                if native.scalar(bs1) and native.scalar(bs2) else\
-                bytes([0])
+            return add(base(bs1), base(bs2)) if scalar(bs1) and scalar(bs2) else bytes([0])
         return check_or_generate_operation(self, fun, [32, 32], bits)
 
     def test_sub(self, bits='002400041000800280800000a5024408111000800000810000008040a0081040'):
         def fun(bs):
             (bs1, bs2) = parts(bs, length=32)
-            return\
-                native.sub(native.base(bs1), native.base(bs2))\
-                if native.scalar(bs1) and native.scalar(bs2) else\
-                bytes([0])
-        return check_or_generate_operation(self, fun, [32, 32], bits)
-
-
-class Test_oblivious_sodium(TestCase):
-    def test_scalar(self, bits='4df8fe738c097afa7f255b10c3ab118eeb73e38935605042ccb7581c73f1e5e9'):
-        def fun(bs):
-            return bitlist([1 if sodium.scalar(bs) else 0])
-        return check_or_generate_operation(self, fun, [32], bits)
-
-    def test_base(self, bits='080874618c0878927620101043a31002e840818101204000401210101261c120'):
-        def fun(bs):
-            return sodium.base(bs) if sodium.scalar(bs) else bytes([0])
-        return check_or_generate_operation(self, fun, [32], bits)
-
-    def test_mul(self, bits='28c5004000000100850000002102088891100000000081080810004280080004'):
-        def fun(bs):
-            (bs1, bs2) = parts(bs, length=32)
-            return\
-                sodium.mul(bs1, sodium.base(bs2))\
-                if sodium.scalar(bs1) and sodium.scalar(bs2) else\
-                bytes([0])
-        return check_or_generate_operation(self, fun, [32, 32], bits)
-
-    def test_add(self, bits='0844000040000002818040008400000031080028000081080801000204081800'):
-        def fun(bs):
-            (bs1, bs2) = parts(bs, length=32)
-            return\
-                sodium.add(sodium.base(bs1), sodium.base(bs2))\
-                if sodium.scalar(bs1) and sodium.scalar(bs2) else\
-                bytes([0])
-        return check_or_generate_operation(self, fun, [32, 32], bits)
-
-    def test_sub(self, bits='002400041000800280800000a5024408111000800000810000008040a0081040'):
-        def fun(bs):
-            (bs1, bs2) = parts(bs, length=32)
-            return\
-                sodium.sub(sodium.base(bs1), sodium.base(bs2))\
-                if sodium.scalar(bs1) and sodium.scalar(bs2) else\
-                bytes([0])
+            return sub(base(bs1), base(bs2)) if scalar(bs1) and scalar(bs2) else bytes([0])
         return check_or_generate_operation(self, fun, [32, 32], bits)
 
 if __name__ == "__main__":
     # Generate reference bit lists for tests.
-    test_oblivious = Test_oblivious_native()
+    test_oblivious = Test_oblivious()
     for m in [m for m in dir(test_oblivious) if m.startswith('test_')]:
         print(m + ': ' + getattr(test_oblivious, m)(bits=None))
