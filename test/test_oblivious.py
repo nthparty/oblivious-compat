08d79a00bf9d70789bbe0266e8f430731db5a907
Andrei Lapets <a@lapets.io>
2020-08-02 23:18:15 -0400

    Fix.

--- test/test_oblivious.py
+++ test/test_oblivious.py
@@ -24,36 +24,36 @@ class Test_oblivious(TestCase):
 
     def test_2(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_box_publickeybytes')
-        self.assertTrue(hasattr(l, 'crypto_box_secretkeybytes')
+        self.assertTrue(hasattr(l, 'crypto_box_publickeybytes'))
+        self.assertTrue(hasattr(l, 'crypto_box_secretkeybytes'))
 
     def test_3(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_bytes')
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_bytes'))
 
     def test_4(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_scalar_random')
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_scalar_random'))
 
     def test_5(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_scalarmult_ristretto255_base')
+        self.assertTrue(hasattr(l, 'crypto_scalarmult_ristretto255_base'))
 
     def test_6(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_scalarmult_ristretto255')
+        self.assertTrue(hasattr(l, 'crypto_scalarmult_ristretto255'))
 
     def test_7(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_add')
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_add'))
 
     def test_8(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_sub')
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_sub'))
 
     def test_9(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
-        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_sub')
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_sub'))
 
     #def test_scalar2(self):
     #    self.assertTrue(sodium is not None)
