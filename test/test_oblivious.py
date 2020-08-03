88c834234da04a7e05c3bdd9fdc49d234c51d9d9
Andrei Lapets <a@lapets.io>
2020-08-02 23:16:32 -0400

    Test.

--- test/test_oblivious.py
+++ test/test_oblivious.py
@@ -18,9 +18,45 @@ def check_or_generate_operation(self, fun, lengths, bits):
     return check_or_generate(self, fs, bits)
 
 class Test_oblivious(TestCase):
-    def test_scalar(self):
+    def test_1(self):
         l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
         self.assertTrue(l is not None)
+
+    def test_2(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_box_publickeybytes')
+        self.assertTrue(hasattr(l, 'crypto_box_secretkeybytes')
+
+    def test_3(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_bytes')
+
+    def test_4(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_scalar_random')
+
+    def test_5(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_scalarmult_ristretto255_base')
+
+    def test_6(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_scalarmult_ristretto255')
+
+    def test_7(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_add')
+
+    def test_8(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_sub')
+
+    def test_9(self):
+        l = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sodium'))
+        self.assertTrue(hasattr(l, 'crypto_core_ristretto255_sub')
+
+    #def test_scalar2(self):
+    #    self.assertTrue(sodium is not None)
 
 if __name__ == "__main__":
     # Generate reference bit lists for tests.
